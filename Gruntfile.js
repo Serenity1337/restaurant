9bc18c127fd917b2a118c4cc35ae775aca856900
unknown <skarolis99@gmail.com>
2019-09-18 15:56:19 +0300

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,33 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+      compass: {                  // Task
+        dist: {                   // Target
+          options: {              // Target options
+            sassDir: 'sass',
+            cssDir: 'css',
+            environment: 'production'
+          }
+        },
+      },
+      watch: {
+        css: {
+          files: ['sass/*.scss'],
+          tasks: ['compass'],
+          options: {
+            livereload: true,
+          },
+        },
+      },
+    });
+  
+    [
+      'grunt-contrib-compass',
+      'grunt-contrib-watch'
+    ].forEach(grunt.loadNpmTasks);
+  
+    grunt.registerTask('default', ['compass']);
+  };
+  
+  
+  
+  
